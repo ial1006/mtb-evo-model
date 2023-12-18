39ca280d29a54a86cc3e340a115c8f80b0d1fa08
rfmarasigan <91927144+rfmarasigan@users.noreply.github.com>
2023-12-18 11:57:38 +0800

    Fixed compartmentalization of agents into two islands
    Agents now bounce off of the midpoint of the graph and are only let through a small percentage of the time related to transmission rate. Agent reproduction rate also reduces as it gets closer to population ceiling
    
    To do:
    Implement a per-island population limit instead of a global one (??)
    Learn to track population size over time and confirm if growth rate is indeed logistic

--- mtb-evo-model.jl
+++ mtb-evo-model.jl
@@ -11,13 +11,15 @@ using Random, LinearAlgebra
 	death_rate::Float64
 	transmit_rate::Float64
 
+	loc::String
 #	energy::Float64
 #	size::Float64
 end
 
 function initialize_model(;
+	# population-related parameters still need tuning
 	init_popul = 1000,
-	popul_limit = 10000,
+	popul_limit = 500000,
 
 #	timer = 0,
 
@@ -26,19 +28,28 @@ function initialize_model(;
 #	rec_rate = 5,
 #	growth_rate = 5,
 	death_rate = 0,
-	transmit_rate = 5,
+	transmit_rate = 30,
+	loc = "bod",
 
-	extent = (100, 100),
+	extent = (200, 200),
 	seed = 42,
 	dt = 1,
 )
     space2d = ContinuousSpace(extent; spacing = 2, periodic = false)
     rng = Random.MersenneTwister(seed)
 
-    model = ABM(Mtb, space2d; rng, scheduler = Schedulers.Randomly())
+	properties = (
+#		numEnv = 0,
+#		numHum = 0,
+		popul_limit = popul_limit,
+		dt = dt,
+	)
+
+    model = ABM(Mtb, space2d; rng, properties)
     for _ in 1:init_popul
 #        vel = Tuple(rand(model.rng, 2) * 0.1 .- 1)
-	vel = sincos(2π * rand(model.rng)) .* 0.01
+	vel = (rand(-1:1), rand(-1:1)) .* 1
+#	vel = sincos(2π * rand(model.rng)) .* 0.01
         add_agent!(
 		model,
 		vel,
@@ -49,6 +60,7 @@ function initialize_model(;
 #		growth_rate,
 		death_rate,
 		transmit_rate,
+		loc,
 
 #		energy,
 #		size,
@@ -65,98 +77,62 @@ function agent_step!(mtb, model)
 	#	- different growth and death rates for "human" and "environment" sections
 	# - complex resource tracking 
 
-	if mtb.pos[1] > 50
+	mtb.vel = (rand(-1:1), rand(-1:1)) .* rand(1:5)
+	if mtb.pos[1] > 100 #
+
+		# Reproduction and death rate on right half of screen (env), rates still need to be tuned
 		mtb.repr_rate = 5
 #		mtb.growth_rate = 1
-		mtb.death_rate = 80
+		mtb.death_rate = 4
 
-#		if (mtb.vel[1] + mtb.pos[1] <= 50) && (rand(0:99) > mtb.transmit_rate)
-#			if (mtb.vel[1] - mtb.vel[1] >= 100)
-#			move_agent!(mtb, (100, (mtb.pos[2] + mtb.vel[2])), model, 1)
-#			move_agent!(mtb, (51., mtb.pos[2]), model)
-#			else
-#				move_agent!(mtb, (mtb.pos[1] - mtb.vel[1], mtb.pos[2] + mtb.vel[2]), model)
-#			end
-#		end
+		# only let through midline transmite_rate% of the time
+		if (mtb.vel[1] + mtb.pos[1] <= 100) && (rand(0:99) > mtb.transmit_rate)
+			mtb.vel = (mtb.vel[1] * -1, mtb.vel[2]) # deflect agent if it would pass through the midline in the next frame if it loses transmit_rate dice roll
+			mtb.loc = "bod"
+		end
+		
+		move_agent!(mtb, model, 1)
+
+		#Randomly kill based on death_rate
+		if rand(0:99) < mtb.death_rate
+			remove_agent!(mtb, model)
+		#Randomly reproduce based on repr_rate
+		elseif rand(0:99) < mtb.repr_rate * (1 - (nagents(model)/model.popul_limit))# && nagents(model) <= 1000000
+			add_agent_pos!(Mtb(nextid(model), mtb.pos, (rand(-1:1), rand(-1:1)) .* 1, mtb.repr_rate, mtb.death_rate, mtb.transmit_rate, mtb.loc), model)
+		end
 
 	else
-		mtb.repr_rate = 80
+		# Reproduction and death rate on left half of screen (human), rates still need to be tuned
+		mtb.repr_rate = 8
 #		mtb.growth_rate = 5
-		mtb.death_rate = 5
+		mtb.death_rate = 7
 
-#		if (mtb.vel[1] + mtb.pos[1] >= 50) && (rand(0:99) > mtb.transmit_rate)
-#			if (mtb.vel[1] - mtb.vel[1] <= 0)
-#			move_agent!(mtb, (0, (mtb.pos[2] + mtb.vel[2])), model, 1)
-#			move_agent!(mtb, (50., mtb.pos[2]), model)
-#			else
-#				move_agent!(mtb, (mtb.pos[1] - mtb.vel[1], mtb.pos[2] + mtb.vel[2]), model)
-#			end
-#		end
+		if (mtb.vel[1] + mtb.pos[1] >= 100) && (rand(0:99) > mtb.transmit_rate)
+			mtb.vel = (mtb.vel[1] * -1, mtb.vel[2])
+			mtb.loc = "env"
+		end
 
+		move_agent!(mtb, model, 1)
+
+		#Randomly kill based on death_rate
+		if rand(0:99) < mtb.death_rate
+			remove_agent!(mtb, model)
+		#Randomly reproduce based on repr_rate
+		elseif rand(0:99) < mtb.repr_rate * (1 - (nagents(model)/model.popul_limit)) # reproduction rate decreases as total agents in model get closer to popul_limit
+			add_agent_pos!(Mtb(nextid(model), mtb.pos, (rand(-1:1), rand(-1:1)) .* 1, mtb.repr_rate, mtb.death_rate, mtb.transmit_rate, mtb.loc), model)
+		end
 	end
-
-
-move_agent!(mtb, model)
-
-	#Randomly kill based on death_rate
-	if rand(0:99) < mtb.death_rate
-		remove_agent!(mtb, model)
-	#Randomly reproduce based on repr_rate
-	elseif rand(0:99) < mtb.repr_rate && nagents(model) <= 10000
-#			replicate!(mtb, model, 0, 0)
-			add_agent_pos!(Mtb(nextid(model), mtb.pos, mtb.vel, mtb.repr_rate, mtb.death_rate, mtb.transmit_rate), model)
-	end
-
-
-
-#=
-    # Obtain the ids of neighbors within the bacterium's visual distance
-    neighbor_ids = nearby_ids(mtb, model, mtb.visual_distance)
-    N = 0
-    match = separate = cohere = (0.0, 0.0)
-    # Calculate behaviour properties based on neighbors
-    for id in neighbor_ids
-        N += 1
-        neighbor = model[id].pos
-        heading = neighbor .- bird.pos
-
-        # `cohere` computes the average position of neighboring birds
-        cohere = cohere .+ heading
-        if euclidean_distance(bird.pos, neighbor, model) < bird.separation
-            # `separate` repels the bird away from neighboring birds
-            separate = separate .- heading
-        end
-        # `match` computes the average trajectory of neighboring birds
-        match = match .+ model[id].vel
-    end
-    N = max(N, 1)
-    # Normalise results based on model input and neighbor count
-    cohere = cohere ./ N .* bird.cohere_factor
-    separate = separate ./ N .* bird.separate_factor
-    match = match ./ N .* bird.match_factor
-    # Compute velocity based on rules defined above
-    bird.vel = (bird.vel .+ cohere .+ separate .+ match) ./ 2
-    bird.vel = bird.vel ./ norm(bird.vel)
-=#
-    # Move bird according to new velocity and speed
 end
 
 using CairoMakie
 
-const bird_polygon = Makie.Polygon(Point2f[(-1, -1), (1, 0), (-1, 1)])
-function bird_marker(b::Mtb)
-    φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
-    rotate_polygon(bird_polygon, φ)
-end
-
 model = initialize_model()
-figure, = abmplot(model; am = bird_marker)
+figure = abmplot(model)
 figure
 
 abmvideo(
     "testing.mp4", model, agent_step!;
-    am = bird_marker,
-    framerate = 20, frames = 2000,
+    framerate = 20, frames = 1000,
     title = "testing"
 )
 
